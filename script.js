507713a4267a23dea49fbe441e8b6c82fbedc698
EmanueleManno <mannus1994@gmail.com>
2023-05-30 14:31:59 +0200

    Fizz, Buzz and FizzBuzz in Console

--- script.js
+++ script.js
@@ -2,6 +2,14 @@
 console.log('JS OK')
 
 //STAMPO I NUMERI DA 1 A 100
-for (let i = 0; i < 100; i++ ){
-    console.log(i + 1);
+for (let i = 1; i <= 100; i++ ){
+
+    if (i % 15 == 0) 
+    console.log("FizzBuzz");
+    else if (i % 3 == 0) 
+    console.log("Fizz");
+    else if (i % 5 == 0) 
+    console.log("Buzz");
+    else
+    console.log(i);
 }
